aeb788830c3a752e98a5aa53284bd0cdb2f23f3b
Rukaroa <rukaroa@gmail.com>
2017-06-30 21:40:34 -0500

    adding server and removed files

---
+++ server.js
@@ -0,0 +1,21 @@
+// Include Server Dependencies
+var express = require("express");
+
+// Create Instance of Express
+var app = express();
+// Sets an initial port. We'll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+
+app.use(express.static("./"));
+
+//get main route
+app.get("/", function(req, res) {
+  console.log("main page");
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
